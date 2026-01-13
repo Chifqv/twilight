0062c5028b8967edf0757d759e05f9c0964329bd
Элина Кокоева <yummysalt@bk.ru>
2026-01-13 22:57:50 +0300

    ВСК ГОТОВО2

--- player.js
+++
@@ -1,8 +0,0 @@
-const backbtn = document.getElementById('back');
-
-
-backbtn.addEventListener('click', (event)  => {
-    const id = event.target.dataset.id; 
-
-    window.location.href = `index.html?id=${id}`;
-});
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//плеер
-const treilerbtn = document.getElementById('treiler');
-
-treilerbtn.addEventListener('click', (event)  => {
-    const id = event.target.dataset.id; 
-
-    window.location.href = `player.html?id=${id}`;
-});
 
 //актеры
 let actorsinfo = [
